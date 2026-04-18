74204eac1859443adaa32e1e0465022ef58a7e23
kimjihwan8 <kimjihwan1592@naver.com>
2026-04-18 19:04:00 +0900

    feat: 헤더 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
-import a from "@/shared/assets/aaa.png"
+import mainAnimal from "@/shared/assets/MainAnimal.png"
 
 export default function Home() {
   return (
     <main>
-      <section className="w-full h-[782px] bg-[#1A1A1A] ">
-        <div className="relative w-fit overflow-hidden">
-          <Image
-            src={a}
-            alt="프로필 사진"
-            placeholder="blur"
-            className="block" 
-          />
-          <div className="absolute inset-0 bg-gradient-to-b from-black/0 via-black/0 via-50% to-black/80 pointer-events-none" />
+      <section className="relative w-full h-195.5 overflow-hidden">
+        <Image
+          src={mainAnimal}
+          alt="카피바라 가족이 물가에 모여있는 모습"
+          placeholder="blur"
+          fill
+          className="object-cover object-center"
+          priority
+        />
+        <div className="absolute inset-0 bg-linear-to-b from-black/0 via-black/0 via-50% to-black/80 pointer-events-none" />
+        <div className="absolute bottom-40 left-30">
+          <p className="text-white text-title-2">가까이서 만나고,</p>
+          <p className="text-white text-title-1">
+            함께 <span className="text-main-green">교감</span>하는 경험
+          </p>
         </div>
-
       </section>
 
       <section className="w-full h-[1000px] bg-white p-20">
